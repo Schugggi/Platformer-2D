4efb15a3b65969858bf414f6995d8fe945f51a11
Schugggi <95489210+Schugggi@users.noreply.github.com>
2022-01-28 16:47:51 +0100

    #4 Menu finished
    Menu is now finished and Player can be picked

--- Platformer-2D/Assets/Scripts/MainMenuController.cs
+++ Platformer-2D/Assets/Scripts/MainMenuController.cs
@@ -4,28 +4,20 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     private bool isArcher;
-    private GameObject archer;
-    private GameObject swordsman;
-    private GameObject playerSpawn;
-
     public void SetPlayer(bool isArcherButton)
     {
         isArcher = isArcherButton;
     }
     public void PlayGame()
     {
-        swordsman = Resources.Load("Prefabs/Player") as GameObject;
-        archer = Resources.Load("Prefabs/Archer") as GameObject;
+        if (!isArcher)
+            PlayerPrefs.SetInt("player", 0);
         if (isArcher)
-            Instantiate(archer, playerSpawn.transform.position, playerSpawn.transform.rotation);
-        else
-            Instantiate(swordsman, playerSpawn.transform.position, playerSpawn.transform.rotation);
+            PlayerPrefs.SetInt("player", 1);
     }
-
     public void LoadNextLevel()
     {
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
-        playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn");
     }
 
     public void QuitGame()
